dbb211b8ef45c1fc1b97a81c65c93662d117b460
Matthew Young <matthew.young@unh.edu>
2021-05-24 14:31:50 -0400

    Update import paths

--- python/seed_spectrum.py
+++ python/seed_spectrum.py
@@ -8,8 +8,8 @@ import matplotlib.lines as mlines
 from matplotlib.legend import Legend
 import numpy as np
 
-import tools
-import seed
+import python.tools as tools
+import python.seed as seed
 
 
 def main(
